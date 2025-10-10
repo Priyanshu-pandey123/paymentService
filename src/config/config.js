163d7b427ccb41aac7b9dc50cd6ac827d01e2827
mac <mac@macs-MacBook-Pro.local>
2025-10-10 00:43:30 -0700

    make the db  crendential  comes  from  env

---
+++ src/config/config.js
@@ -0,0 +1,29 @@
+// config/config.js
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_DEV,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql',
+    timezone: process.env.DB_TIMEZONE || '+05:30',
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql',
+    timezone: process.env.DB_TIMEZONE || '+05:30',
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql',
+    timezone: process.env.DB_TIMEZONE || '+05:30',
+  },
+};
--- src/models/index.js
+++ src/models/index.js
@@ -3,10 +3,11 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+require('dotenv').config(); // load environment variables
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env]; // changed .json to .js
 const db = {};
 
 let sequelize;
