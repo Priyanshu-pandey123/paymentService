991775f9ac58908b654fb24f3fb14b0eb67ab93e
mac <mac@macs-MacBook-Pro.local>
2025-10-27 02:26:14 -0700

    fix  the db name

--- src/config/config.js
+++ src/config/config.js
@@ -12,7 +12,7 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME || '',
     password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || '',
+    database: process.env.DB_NAME_DEV || '',
     host: process.env.DB_HOST || "",
     dialect: process.env.DB_DIALECT || 'mysql',
     timezone: process.env.DB_TIMEZONE || '+05:30',
@@ -20,7 +20,7 @@ module.exports = {
   test: {
     username: process.env.DB_USERNAME  || '',
     password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || '',
+    database: process.env.DB_NAME_DEV || '',
     host: process.env.DB_HOST || "",
     dialect: process.env.DB_DIALECT || 'mysql',
     timezone: process.env.DB_TIMEZONE || '+05:30',
@@ -28,7 +28,7 @@ module.exports = {
   production: {
     username: process.env.DB_USERNAME  || '',
     password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || '',
+    database: process.env.DB_NAME_DEV || '',
     host: process.env.DB_HOST || "",
     dialect: process.env.DB_DIALECT || 'mysql',
     timezone: process.env.DB_TIMEZONE || '+05:30',
